3a6a9a8fe78f5bdb4bf169ab9c914d7a9b1b7b00
studud <71371760+studud@users.noreply.github.com>
2021-12-14 12:00:50 +0200

    kwks

--- public/js/profile.js
+++ public/js/profile.js
@@ -13,7 +13,7 @@ const section = document.querySelector('#profile-info');
 const user = JSON.parse(sessionStorage.getItem('user'));
 
 const profileInfo = () => {
-
+  console.log(user)
 
 
     const profileName = document.querySelector('#profile-name')
